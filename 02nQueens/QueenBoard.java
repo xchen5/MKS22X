4d1cab337e801c1bc2d282c1a0d725412b8e47ff
Xavier Chen <xchen5@stuy.edu>
2017-02-13 12:31:41 -0500

    AddQueen

--- 02nQueens/QueenBoard.java
+++ 02nQueens/QueenBoard.java
@@ -16,13 +16,25 @@ public class QueenBoard{
      *final configuration of the board after adding
      *all n queens. Uses solveH
      */
-/*    public boolean solve()
+    public boolean solve()
     {
 	return solveH(0);
     }
+    
+    /* public int getCount() {
+    return solutionCount;
+    } */
 
     private boolean solveH(int col){
-	return false;
+	if (col == board.length){
+	    return true;
+	}
+	for(int row = 0; row < board.length; row++){
+	    if (board[row][col] == 0) {
+		addQueen(row, col);
+	    }
+	}
+        return solveH(col += 1);
     }
 
     /**
@@ -54,26 +66,58 @@ public class QueenBoard{
       for (int i = c; i >= 0; i--) {
         board[r][i] += 1;
       }
-      //diaganol
-      for( int i = r ; i < board.length; i++) {
-        int ci = c;
-        if (ci < board.length) {
-        board[i][ci] +=1;
-        ci++;
+      //diagonal
+      for( int i = 0 ;(r + i < board.length)&&(c + i < board.length); i++) {
+	  board[r+i][c+i]+=1;
       }
-
-    }
+      for( int i = 0 ;(r + i >= 0)&&(c + i >= 0); i--) {
+	  board[r+i][c+i]+=1;
+      }
+      for( int i = 0 ;(r + i < board.length)&&(c + i > 0); i++) {
+	  board[r+i][c-i]+=1;
+      }
+      for( int i = 0 ;(r+i > 0)&&(c + i <board.length); i++) {
+	  board[r-i][c+i]+=1;
+      }
       board[r][c] = -1;
 
 
     }
-  /*  private void removeQueen(int r, int c)
+    private void removeQueen(int r, int c) {
+	//up down
+      for (int i = r; i <board.length; i++) {
+        board[i][c] -= 1;
+      }
+      for (int i = r; i >= 0; i--) {
+        board[i][c] -= 1;
+      }
+      //left right
+      for (int i = c; i <board[r].length; i++) {
+        board[r][i] -= 1;
+      }
+      for (int i = c; i >= 0; i--) {
+        board[r][i] -= 1;
+      }
+      //diagonal
+      for( int i = 0 ;(r + i < board.length)&&(c + i < board.length); i++) {
+	  board[r+i][c+i]-=1;
+      }
+      for( int i = 0 ;(r + i >= 0)&&(c + i >= 0); i--) {
+	  board[r+i][c+i]-=1;
+      }
+      for( int i = 0 ;(r + i < board.length)&&(c + i > 0); i++) {
+	  board[r+i][c-i]-=1;
+      }
+      for( int i = 0 ;(r+i > 0)&&(c + i <board.length); i++) {
+	  board[r-i][c+i]-=1;
+      }
+      board[r][c] = 0;
 
-} */
+} 
 
   public static void main(String[] args) {
       QueenBoard a = new QueenBoard(5);
-      a.addQueen(2,2);
+      a.addQueen(0,3);
       System.out.println(Arrays.deepToString(a.board).replace("], ", "]\n"));
   }
 }
