baf796bf74b747df3fbb318aee1324b41a1056bf
xchen5 <xchen5@stuy.edu>
2017-02-21 17:54:22 -0500

    FINISHED!!!

--- 03KnightTour/KnightBoard.java
+++ 03KnightTour/KnightBoard.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 public class KnightBoard {
   private int[][]board;
 
@@ -15,56 +14,84 @@ public class KnightBoard {
     board[row][col] = number;
   }
 
-    private boolean isSafe(int row, int col) {
-      if ( (row >= 0) &&
-           (col >= 0) &&
-           (row < board.length) &&
-           (col < board[0].length)&&
-           (board[row][col] == 0)){
-        return true;
-      }
-      else
-      {
-        return false;
-      }
+  private boolean isSafe(int row, int col) {
+    if ( (row >= 0) &&
+    (col >= 0) &&
+    (row < board.length) &&
+    (col < board[0].length)&&
+    (board[row][col] == 0)){
+      return true;
     }
-
-    private boolean solveH(int row, int col, int level){
-      if (level > (board.length * board[0].length)) {
-        return true;
-      }
-      else {
-        if (isSafe(row, col)) {
-          addKnight(row, col, level);
-          if (solveH(row + 1, col + 2, level++) ||
-          solveH(row + 1, col - 2, level++) ||
-          solveH(row - 1, col + 2, level++) ||
-          solveH(row - 1, col - 2, level++) ||
-          solveH(row + 2, col + 1, level++) ||
-          solveH(row + 2, col - 1, level++) ||
-          solveH(row - 2, col - 1, level++) ||
-          solveH(row - 2, col + 1, level++)) {
-            return true;
-          }
-          board[row][col] = 0;
-        }
-
-      }
+    else
+    {
       return false;
     }
+  }
 
-    public void solve(){
-      solveH(0, 0, 1);
+  private boolean solveH(int row, int col, int level){
+    if (level > (board.length * board[0].length)) {
+      return true;
+    }
+    if (isSafe(row, col)) {
+      addKnight(row, col, level);
+      if (solveH(row + 1, col + 2, level + 1) ||
+      solveH(row + 1, col - 2, level + 1) ||
+      solveH(row - 1, col + 2, level + 1) ||
+      solveH(row - 1, col - 2, level + 1) ||
+      solveH(row + 2, col + 1, level + 1) ||
+      solveH(row + 2, col - 1, level + 1) ||
+      solveH(row - 2, col - 1, level + 1) ||
+      solveH(row - 2, col + 1, level + 1)) {
+        return true;
+      }
+      board[row][col] = 0;
+    }
+    return false;
+  }
+
+  public void solve(){
+    solveH(0, 0, 1);
+  }
+
+  public boolean doubleDigit() {
+    boolean nah = false;
+    for (int r = 0; r<board.length;r++){
+      for(int c= 0; c<board[0].length; c++) {
+        if (board[r][c] >= 10) {
+          nah = true;
+        }
+      }
+    }
+    return nah;
+  }
+
+  public String toString( ){
+    String temp = "";
+    boolean doublee = doubleDigit();
+    for (int r = 0; r < board.length; r++){
+      if (r >= 1) {
+        temp += "\n";}
+        for (int c = 0; c < board[0].length; c++) {
+          if (doublee) {
+            if (board[r][c] < 10) {
+              temp += " " + board[r][c] + " ";
+            }
+            else {
+              temp += board[r][c] + " ";
+            }
+          }
+          else {
+            temp += board[r][c] + " ";
+          }
+        }
+      }
+      return temp;
     }
 
-    public String toString( ){
-      return Arrays.deepToString(board).replace("], ", "]\n");
-    }
-
-    public static void main(String[]args){
-      KnightBoard a = new KnightBoard(7,7);
-      System.out.println(a.solveH(0, 0, 1));
+    /*public static void main(String[]args){
+      KnightBoard a = new KnightBoard(7,7 );
+      a.solve();
       System.out.println(a.toString());
-    }
+    }*/
 
   }
