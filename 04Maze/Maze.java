d6b0e87232ffd8415dc145a4463ab8ec3d393d48
Xavier Chen <xchen5@stuy.edu>
2017-02-28 12:36:14 -0500

    Almost got the chars in the array

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -19,15 +19,41 @@ public class Maze{
     public Maze(String filename){
 	try {
 	    Scanner sc = new Scanner(new File(filename));
-	    while(sc.hasNextLine()){
-		
-	    }
+	    int r = 0;
+	    int c = 0;
+	   
+	     while(sc.hasNextLine()){
+		r += 1;
+		c += sc.nextLine().length();
+		}
+	     c = c/r;
+	     maze = new char[r][c];
+	     
+	     System.out.println("r:" + r +", c:" + c);
+	     System.out.println("# of chars: " + (maze.length * maze[1].length));
+	     //Putting the chars into the array
+	     int row = 0;
+	     while(sc.hasNextLine()){
+		 row += 1;
+		 for(int col = 0;col < c; col++) {
+		     maze[row][col] = (sc.nextLine()).charAt(col);
+		 }
+	     }
+	     System.out.println(Arrays.deepToString(maze));
 	}
 	catch(FileNotFoundException e) {
 	    System.out.println("File not found");
 	}
 
     }
+
+    /*    private void wait(int millis){ //ADDED SORRY!
+         try {
+             Thread.sleep(millis);
+         }
+         catch (InterruptedException e) {
+         }
+     }
 
     public void setAnimate(boolean b){
         animate = b;
@@ -42,12 +68,12 @@ public class Maze{
     /*Wrapper Solve Function
       Since the constructor exits when the file is not found or is missing an E or S, we can assume it exists.
     */
-    public boolean solve(){
+    /* public boolean solve(){
             int startx=0,starty=0;
             //Initialize startx and starty with the location of the S. 
             maze[startx][starty] = ' ';//erase the S, and start solving!
             return solve(startx,starty);
-    }
+    } */
 
     /*
       Recursive Solve function:
@@ -62,7 +88,7 @@ public class Maze{
         All visited spots that were not part of the solution are changed to '.'
         All visited spots that are part of the solution are changed to '@'
     */
-    private boolean solve(int x, int y){
+    /* private boolean solve(int x, int y){
         if(animate){
             System.out.println("\033[2J\033[1;1H"+this);
             wait(20);
@@ -70,6 +96,10 @@ public class Maze{
 
         //COMPLETE SOLVE
         return false; //so it compiles
+	} */
+    public static void main (String[]args) {
+	Maze f;
+	f = new Maze("test.txt");
     }
 
 }
