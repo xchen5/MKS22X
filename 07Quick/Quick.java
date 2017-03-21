3d888f27ad2e8eaa4f462c2a96aceee0b03dc6f9
Xavier Chen <xchen5@stuy.edu>
2017-03-21 17:28:55 -0400

    started dutch flag

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Quick {
-  public static int part ( int [] data, int start, int end){
+    /* public static int part ( int [] data, int start, int end){
     //-Choose a random element to be a pivot, and partition the array around it.
     //-Only partition the elements from start to end inclusive.
     //-When done returns the index of the final position of the pivot element.
@@ -35,8 +35,43 @@ public class Quick {
     }
     System.out.print("\n");
     return (startCounter + 1);
-  }
+  } */
 
+    public static int part(int[]data,int start, int end){
+	int lt = start + 1;
+	int gt = end;
+	int i = start + 2;
+	int pivot = (int) (Math.random() * end) ;
+	int pivotValue = data[pivot];
+	System.out.println(pivotValue);
+	swap(data, pivot, 0);
+	while (i<=gt) {
+	    if (data[i] == pivotValue){
+		i++;
+	    }
+	    else if(data[i] < pivotValue)
+		{
+		    swap(data, i, lt);
+		    lt++;
+		    i++;
+		}
+	    else {	
+		swap(data, i, gt);
+		gt--;
+	    }
+	}
+	swap(data, 0, lt + 1);
+	System.out.println(Arrays.toString(data));
+	return lt + 1;
+      
+    }
+
+    public static void swap(int[]data, int ind1, int ind2){
+	int tempInd1 = data[ind2];
+	int tempInd2 = data[ind1];
+	data[ind1] = tempInd1;
+	data[ind2] = tempInd2;
+    }
   public static int quickselect(int[]data, int k){
     int pivot = part(data, -1, data.length - 1);
     if (k == pivot) {
@@ -48,7 +83,7 @@ public class Quick {
 
   public static void main(String[]args){
     int[]ary={999,999,999,4,1,0,3,2,999,999,999};
-    System.out.println("a");
+    System.out.println(part(ary,0,ary.length - 1));
   }
 
 }
