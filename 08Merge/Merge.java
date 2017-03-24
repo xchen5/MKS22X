bd4125ed23f73167046824158c2391f0bef94f7f
Xavier Chen <xchen5@stuy.edu>
2017-03-24 15:31:03 -0400

    started merge

---
+++ 08Merge/Merge.java
@@ -0,0 +1,11 @@
+public class Merge {
+    public static void merge(int[]a,int[]b,int[]destination) {
+	
+    }
+    public static void main(String[]args){
+	int[]a = {0,2,4,6,8};
+	int[]b = {1,3,5,7,9};
+	int[]c = new int[10];
+	merge(a,b,c);
+    }
+}
