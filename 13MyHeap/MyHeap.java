88ae03e3691e77ca4b0763cf46645fe6db88927a
Xavier Chen <xchen5@stuy.edu>
2017-05-08 12:29:08 -0400

    worked on heap

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -15,6 +15,7 @@ public class MyHeap {
 
     public void add(String s){
 	heap.add(s);
+	pushUp();
     }
 
     public String toString(){
@@ -34,10 +35,23 @@ public class MyHeap {
 	return (index / 2) > 0; 
     }
 
+    private void swap(int ind1, int ind2){
+	String firstval =  heap.get(ind1);
+	String secondval = heap.get(ind2);
+	heap.set(ind1, secondval);
+	heap.set(ind2, firstval);
+    }
+
+    private void pushUp(){
+	if(heap.get(heap.size()-1).compareTo(heap.get((heap.size()-1) / 2)) < 0 ){
+	    swap( heap.size()-1 , (heap.size()-1)/2);
+	}
+    }
     public static void main(String[]args){
 	MyHeap a = new MyHeap();
-	a.add("hi");
 	a.add("x");
+	a.add("hi");
+	a.add("bye");
 	System.out.println(a);
     }
 }
