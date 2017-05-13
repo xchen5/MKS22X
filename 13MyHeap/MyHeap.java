00a28c85ae792e10c9337e90d5599f5e4e87e873
xchen5 <xchen5@stuy.edu>
2017-05-13 19:53:09 -0400

    fixed all problems for heap!

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,85 +1,131 @@
 import java.util.*;
 public class MyHeap {
-    private ArrayList<String> heap = new ArrayList<String>();
-    private boolean isMax;
+  private ArrayList<String> heap = new ArrayList<String>();
+  private boolean isMax;
 
-    public MyHeap(){
-	heap.add("");
-	isMax = true;
+  public MyHeap(){
+    heap.add("");
+    isMax = true;
+  }
+
+  public MyHeap(boolean x){
+    heap.add("");
+    isMax = x;
+  }
+
+  public void add(String s){
+    heap.add(s);
+    pushUp(heap.size() - 1);
+  }
+
+  public String remove(){
+    String ret = heap.get(1);
+    heap.set(1, heap.remove(heap.size() - 1));
+    pushDown(1);
+    return ret;
+  }
+
+  public String toString(){
+    String ret = "[";
+    for(int i = 1; i < heap.size(); i++){
+      if(i == heap.size() - 1){
+        ret += heap.get(i) + "";
+      }
+      else{
+        ret += heap.get(i) + ", ";
+      }
     }
+    return ret + "]";
+  }
 
-    public MyHeap(boolean x){
-	heap.add("");
-	isMax = x;
-    }
+  private boolean hasParent(int index){
+    return (index / 2) > 0;
+  }
 
-    public void add(String s){
-	heap.add(s);
-	pushUp(heap.size() - 1);
-    }
+  private boolean hasChildren(int index) {
+    return (index * 2) < heap.size();
+  }
 
-    public String remove(){
-      String ret = heap.get(1);
-      heap.set(1, heap.remove(heap.size() - 1));
-      pushDown(1);
-      return ret;
-    }
+  private boolean hasTwoChildren(int index){
+    return (index * 2) + 1< heap.size();
+  }
 
-    public String toString(){
-	String ret = "[";
-	for(int i = 1; i < heap.size(); i++){
-	    if(i == heap.size() - 1){
-		ret += heap.get(i);
-	    }
-	    else{
-		ret += heap.get(i) + ", ";
-	    }
-	}
-	return ret + "]";
-    }
+  private String getRight(int index){
+    return heap.get((index * 2) + 1);
+  }
 
-    private boolean hasParent(int index){
-	return (index / 2) > 0;
-    }
+  private String getLeft(int index){
+    return heap.get(index * 2);
+  }
 
-    private boolean hasChildren(int index) {
-      return (index * 2) < heap.size();
-    }
+  private void swap(int ind1, int ind2){
+    String firstval =  heap.get(ind1);
+    String secondval = heap.get(ind2);
+    heap.set(ind1, secondval);
+    heap.set(ind2, firstval);
+  }
 
-    private void swap(int ind1, int ind2){
-	String firstval =  heap.get(ind1);
-	String secondval = heap.get(ind2);
-	heap.set(ind1, secondval);
-	heap.set(ind2, firstval);
-    }
-
-    private void pushDown(int index){
-      if (isMax == false){
-      while(hasChildren(index)){
-        if(heap.get(index).compareTo(heap.get(index * 2)) >  0){
-          swap( index, index * 2);
-          pushUp(index * 2);
+  private void pushDown(int index){
+    if (!isMax){
+      if(hasTwoChildren(index)){
+        if((heap.get(index).compareTo(getLeft(index)) > 0) && (heap.get(index).compareTo(getRight(index)) > 0)){
+          if(getLeft(index).compareTo(getRight(index)) < 0){
+            swap(index, index * 2);
+            pushDown(index * 2);
+          }
+          else {
+            swap(index, (index * 2) + 1);
+            pushDown((index * 2) + 1);
+          }
         }
-        else{
-          return;
+        else if(heap.get(index).compareTo(getLeft(index)) > 0){
+          swap(index, (index * 2));
+          pushDown(index * 2);
+        }
+        else if(heap.get(index).compareTo(getRight(index)) > 0){
+          swap(index, (index * 2) + 1);
+          pushDown((index * 2) + 1);
+        }
+      }
+      else if (hasChildren(index)){
+        if(heap.get(index).compareTo(getLeft(index)) > 0){
+          swap(index, index * 2);
+          pushDown(index * 2);
         }
       }
     }
     else{
-      while(hasChildren(index)){
-        if(heap.get(index).compareTo(heap.get(index * 2)) <  0){
-          swap( index, index * 2);
-          pushUp(index * 2);
+      if(hasTwoChildren(index)){
+        if((heap.get(index).compareTo(getLeft(index)) < 0) && (heap.get(index).compareTo(getRight(index)) < 0)){
+          if(getLeft(index).compareTo(getRight(index)) > 0){
+            swap(index, index * 2);
+            pushDown(index * 2);
+          }
+          else {
+            swap(index, (index * 2) + 1);
+            pushDown((index * 2) + 1);
+          }
         }
-        else{
-          return;
+        else if(heap.get(index).compareTo(getLeft(index)) < 0){
+          swap(index, (index * 2));
+          pushDown(index * 2);
+        }
+        else if(heap.get(index).compareTo(getRight(index)) < 0){
+          swap(index, (index * 2) + 1);
+          pushDown((index * 2) + 1);
+        }
+      }
+      else if (hasChildren(index)){
+        if(heap.get(index).compareTo(getLeft(index)) < 0){
+          swap(index, index * 2);
+          pushDown(index * 2);
         }
       }
     }
   }
 
-    private void pushUp(int index){
-      if (isMax == false) {
+  private void pushUp(int index){
+    if (!isMax) {
       while(hasParent(index)){
         if(heap.get(index).compareTo(heap.get(index / 2)) < 0){
           swap( index, index / 2);
@@ -103,17 +149,11 @@ public class MyHeap {
     }
   }
 
-    public String peek(){
-      return heap.get(1);
-    }
-    public static void main(String[]args){
-	MyHeap a = new MyHeap(false);
-  a.add("x");
-	a.add("y");
-	a.add("z");
-  a.add("w");
-  a.add("a");
-  a.remove();
-	System.out.println(a);
-    }
+  public String peek(){
+    return heap.get(1);
+  }
+
+  public int getSize(){
+    return heap.size() - 1;
+  }
 }
--- 14RunningMedian/MyHeap.java
+++ 14RunningMedian/MyHeap.java
@@ -1,75 +1,131 @@
 import java.util.*;
 public class MyHeap {
-    private ArrayList<Integer> heap = new ArrayList<Integer>();
-    private boolean isMax;
+  private ArrayList<Integer> heap = new ArrayList<Integer>();
+  private boolean isMax;
 
-    public MyHeap(){
-	heap.add(0);
-	isMax = true;
+  public MyHeap(){
+    heap.add(0);
+    isMax = true;
+  }
+
+  public MyHeap(boolean x){
+    heap.add(0);
+    isMax = x;
+  }
+
+  public void add(Integer s){
+    heap.add(s);
+    pushUp(heap.size() - 1);
+  }
+
+  public Integer remove(){
+    Integer ret = heap.get(1);
+    heap.set(1, heap.remove(heap.size() - 1));
+    pushDown(1);
+    return ret;
+  }
+
+  public String toString(){
+    String ret = "[";
+    for(int i = 1; i < heap.size(); i++){
+      if(i == heap.size() - 1){
+        ret += heap.get(i) + "";
+      }
+      else{
+        ret += heap.get(i) + ", ";
+      }
     }
+    return ret + "]";
+  }
 
-    public MyHeap(boolean x){
-	heap.add(0);
-	isMax = x;
-    }
+  private boolean hasParent(int index){
+    return (index / 2) > 0;
+  }
 
-    public void add(Integer s){
-	heap.add(s);
-	pushUp(heap.size() - 1);
-    }
+  private boolean hasChildren(int index) {
+    return (index * 2) < heap.size();
+  }
 
-    public Integer remove(){
-      Integer ret = heap.get(1);
-      heap.set(1, heap.remove(heap.size() - 1));
-      pushDown(1);
-      return ret;
-    }
+  private boolean hasTwoChildren(int index){
+    return (index * 2) + 1< heap.size();
+  }
 
-    public String toString(){
-	String ret = "[";
-	for(int i = 1; i < heap.size(); i++){
-	    if(i == heap.size() - 1){
-		ret += heap.get(i) + "";
-	    }
-	    else{
-		ret += heap.get(i) + ", ";
-	    }
-	}
-	return ret + "]";
-    }
+  private Integer getRight(int index){
+    return heap.get((index * 2) + 1);
+  }
 
-    private boolean hasParent(int index){
-	return (index / 2) > 0;
-    }
+  private Integer getLeft(int index){
+    return heap.get(index * 2);
+  }
 
-    private boolean hasChildren(int index) {
-      return (index * 2) < heap.size();
-    }
+  private void swap(int ind1, int ind2){
+    Integer firstval =  heap.get(ind1);
+    Integer secondval = heap.get(ind2);
+    heap.set(ind1, secondval);
+    heap.set(ind2, firstval);
+  }
 
-    private void swap(int ind1, int ind2){
-	Integer firstval =  heap.get(ind1);
-	Integer secondval = heap.get(ind2);
-	heap.set(ind1, secondval);
-	heap.set(ind2, firstval);
-    }
-
-    private void pushDown(int index){
-      if (isMax == false){
-        if(hasChildren(index) && (heap.get(index).compareTo(heap.get(index * 2)) >  0)){
-          swap( index, index * 2);
+  private void pushDown(int index){
+    if (!isMax){
+      if(hasTwoChildren(index)){
+        if((heap.get(index).compareTo(getLeft(index)) > 0) && (heap.get(index).compareTo(getRight(index)) > 0)){
+          if(getLeft(index).compareTo(getRight(index)) < 0){
+            swap(index, index * 2);
+            pushDown(index * 2);
+          }
+          else {
+            swap(index, (index * 2) + 1);
+            pushDown((index * 2) + 1);
+          }
+        }
+        else if(heap.get(index).compareTo(getLeft(index)) > 0){
+          swap(index, (index * 2));
           pushDown(index * 2);
         }
+        else if(heap.get(index).compareTo(getRight(index)) > 0){
+          swap(index, (index * 2) + 1);
+          pushDown((index * 2) + 1);
+        }
+      }
+      else if (hasChildren(index)){
+        if(heap.get(index).compareTo(getLeft(index)) > 0){
+          swap(index, index * 2);
+          pushDown(index * 2);
+        }
+      }
     }
     else{
-        if(hasChildren(index) && (heap.get(index).compareTo(heap.get(index * 2)) <  0) ){
-          swap( index, index * 2);
+      if(hasTwoChildren(index)){
+        if((heap.get(index).compareTo(getLeft(index)) < 0) && (heap.get(index).compareTo(getRight(index)) < 0)){
+          if(getLeft(index).compareTo(getRight(index)) > 0){
+            swap(index, index * 2);
+            pushDown(index * 2);
+          }
+          else {
+            swap(index, (index * 2) + 1);
+            pushDown((index * 2) + 1);
+          }
+        }
+        else if(heap.get(index).compareTo(getLeft(index)) < 0){
+          swap(index, (index * 2));
           pushDown(index * 2);
         }
+        else if(heap.get(index).compareTo(getRight(index)) < 0){
+          swap(index, (index * 2) + 1);
+          pushDown((index * 2) + 1);
+        }
+      }
+      else if (hasChildren(index)){
+        if(heap.get(index).compareTo(getLeft(index)) < 0){
+          swap(index, index * 2);
+          pushDown(index * 2);
+        }
+      }
     }
   }
 
-    private void pushUp(int index){
-      if (isMax == false) {
+  private void pushUp(int index){
+    if (!isMax) {
       while(hasParent(index)){
         if(heap.get(index).compareTo(heap.get(index / 2)) < 0){
           swap( index, index / 2);
@@ -93,23 +149,23 @@ public class MyHeap {
     }
   }
 
-    public Integer peek(){
-      return heap.get(1);
-    }
+  public Integer peek(){
+    return heap.get(1);
+  }
 
-    public int getSize(){
-      return heap.size() - 1;
+  public int getSize(){
+    return heap.size() - 1;
+  }
+  public static void main(String[]args){
+    MyHeap a = new MyHeap(true);
+    for(int i = 0; i < 10; i++){
+      a.add(i);
+      System.out.println(a);
     }
-    public static void main(String[]args){
-	MyHeap a = new MyHeap(false);
-  for(int i = 0; i < 10; i++){
-    a.add(i);
+    for(int i = 0; i < 9; i++){
+      a.remove();
+      System.out.println(a);
+    }
     System.out.println(a);
   }
-  for(int i = 0; i < 9; i++){
-    a.remove();
-    System.out.println(a);
-  }
-	System.out.println(a);
-    }
 }
